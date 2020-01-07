662f6f8a3f9643543a77129d48daa93808c20e4d
Irine Octaviana <chayouw_gurlz@yahoo.com>
2020-01-07 14:57:54 +0700

    + fragment movie & favorite

---
+++ app/src/main/java/com/ciaocollect/trendmovies/ui/movie/MovieFragment.java
@@ -0,0 +1,35 @@
+package com.ciaocollect.trendmovies.ui.movie;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.ciaocollect.trendmovies.R;
+
+public class MovieFragment extends Fragment {
+
+    private MovieViewModel movieViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        movieViewModel =
+                ViewModelProviders.of(this).get(MovieViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_movie, container, false);
+        final TextView textView = root.findViewById(R.id.text_movie);
+        movieViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
